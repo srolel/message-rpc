2e083142ecf6bf7c95e195a40d18cfe0543db187
Sharon (Sean) Rolel <srolel@coursera.org>
2020-05-15 00:32:11 -0400

    add tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -13,11 +13,89 @@ describe('MessageRPC', () => {
         },
     };
 
-    it('should  pass the message to receiver', () => {
+    it('basic', () => {
         let counter = 0;
         const rpc = new MessageRPC<{ count: () => void }>({
             count: (): number => counter++,
         });
+
+        rpc.register(reciever);
+        const client = rpc.client(sender);
+
+        client.count();
+
+        expect(counter).toEqual(1);
+    });
+
+    it('with extend', () => {
+        let counter = 0;
+        const rpc = new MessageRPC<{ count: () => void }>();
+
+        rpc.extend({
+            count: (): number => counter++,
+        });
+
+        rpc.register(reciever);
+        const client = rpc.client(sender);
+
+        client.count();
+
+        expect(counter).toEqual(1);
+    });
+
+    it('multiple rpcs', () => {
+        let counter = 0;
+        const rpc1 = new MessageRPC<{ count: () => void }>({
+            count: (): number => counter++,
+        });
+        const rpc2 = new MessageRPC<{ count: () => void }>({
+            count: (): number => counter++,
+        });
+
+        rpc1.register(reciever);
+        rpc1.client(sender).count();
+
+        rpc2.register(reciever);
+        rpc2.client(sender).count();
+
+        expect(counter).toEqual(2);
+    });
+
+    it('multiple clients', () => {
+        let counter = 0;
+        const rpc = new MessageRPC<{ count: () => void }>({
+            count: (): number => counter++,
+        });
+
+        rpc.register(reciever);
+        rpc.client(sender).count();
+        rpc.client(sender).count();
+
+        expect(counter).toEqual(2);
+    });
+
+    it('multiple calls', () => {
+        let counter = 0;
+        const rpc = new MessageRPC<{ count: () => void }>({
+            count: (): number => counter++,
+        });
+
+        rpc.register(reciever);
+        const client = rpc.client(sender);
+        client.count();
+        client.count();
+
+        expect(counter).toEqual(2);
+    });
+
+    it('setting tag', () => {
+        let counter = 0;
+        const rpc = new MessageRPC<{ count: () => void }>(
+            {
+                count: (): number => counter++,
+            },
+            { tag: 'tag' },
+        );
 
         rpc.register(reciever);
         const client = rpc.client(sender);
